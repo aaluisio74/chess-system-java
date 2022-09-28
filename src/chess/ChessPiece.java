379b02fd881b709a034b38e4190aac5fe443804a
Aluisio Anderson <aaluisio@gmail.com>
2022-09-28 10:51:03 -0300

    Implementing possible moves of Rook

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 
@@ -15,5 +16,14 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
-
+	
+	//PROTECTED para ser acessível apenas no pacote (chess) e pelas subclasses (chess.piece) das peças (King e Rook)
+	protected boolean isThereOpponentPiece(Position position) {
+		/*Recebe a peça que está na posição do parâmetro. Aqui teremos um downcasting: é quando o objeto se passa
+		  como se fosse um subtipo dele. Desce a hierarquia.
+		  Upcasting: é quando uma superclasse recebe uma referência da subclasse. Sobe a hierarquia.*/
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		//Retorna se o objeto for diferente de nulo e se a cor da peça é diferente da cor da peça adversária.
+		return p != null && p.getColor() != color;
+	}
 }
